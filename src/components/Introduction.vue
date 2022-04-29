12c6382251afa3f1d9af837be6257836dd560f4a
Mete Alp Kizilcay <kizilcaymetealp@gmail.com>
2022-04-29 14:35:23 +0200

    check deployment

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -3,7 +3,7 @@
         <p id="introTxt">
             Hello, 
             <br>
-            I am a full stack developer with a couple of production experiences. I write code detail oriented, well documented and tested or as fast as greased lightning. I currently seeking to take next career step with a growing startup.
+            I am a computer scientist with a couple of production experiences. I write code detail oriented, well documented and tested or as fast as greased lightning. I currently seeking to take next career step with a growing startup.
         </p>
     </div>
 </template>
